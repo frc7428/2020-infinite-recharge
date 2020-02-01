cf31356d1ec79a57a56fbad933106b9a29092ee0
ch24063 <ch24063@AL.FPCSK12.COM>
2020-02-01 11:00:06 -0600

    correct motor controller

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -16,10 +16,10 @@ package frc.robot;
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
-    public static final int FRONT_LEFT_CAN_ID = 1;
+    public static final int FRONT_LEFT_CAN_ID = 0;
     public static final int FRONT_RIGHT_CAN_ID = 2;
-    public static final int REAR_LEFT_CAN_ID = 3;
-    public static final int REAR_RIGHT_CAN_ID = 4;
+    public static final int REAR_LEFT_CAN_ID = 1;
+    public static final int REAR_RIGHT_CAN_ID = 3;
 
     public static final int DRIVE_JOYSTICK_USB_ID = 0;
     
