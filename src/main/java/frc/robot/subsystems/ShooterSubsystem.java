9388064b807c442cd31b5071a1d1fa9ad37ccd1d
ch24063 <ch24063@AL.FPCSK12.COM>
2020-02-04 16:29:32 -0600

    created shooter subsystem

---
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -0,0 +1,29 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class ShooterSubsystem extends SubsystemBase {
+  private final CANSparkMax pewPew = new CANSparkMax(Constants.SHOOTER_BUTTON, MotorType.kBrushless);
+
+  /**
+   * Creates a new ShooterSubsystem.
+   */
+  public ShooterSubsystem() {
+
+  }
+  public void shooter(boolean pew) {
+    pewPew.set(pew ? 1 : 0);
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
